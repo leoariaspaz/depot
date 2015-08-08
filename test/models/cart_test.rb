ac3b4bb93b1790f73882830da5478d122498d8b9
Leonardo Arias Paz <leoariaspaz@gmail.com>
2015-08-08 19:58:41 -0300

    A Smarter Cart
    Capítulo 10: "A Smarter Cart". Playtime.

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -4,4 +4,28 @@ class CartTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "add unique products" do
+  	cart = Cart.new
+  	ruby_book = products(:ruby).id
+  	cart.add_product(ruby_book)
+  	one_book = products(:one).id
+  	cart.add_product(one_book)
+  	cart.save
+  	assert_equal 1, sum_quantity_in_cart(cart, ruby_book)
+  	assert_equal 1, sum_quantity_in_cart(cart, one_book)
+  end
+
+  test "add duplicate products" do
+  	cart = Cart.new
+  	ruby_book = products(:ruby).id
+  	cart.add_product(ruby_book)
+  	cart.add_product(ruby_book)
+  	cart.save
+  	assert_equal 2, sum_quantity_in_cart(cart, ruby_book)
+  end
+
+  def sum_quantity_in_cart(cart, product_id)  	
+  	cart.line_items.where(product_id: product_id).to_a.sum { |item| item.quantity }
+  end
 end
